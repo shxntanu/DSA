128e044345203d148735fe7a32dcaf1f8cda96b9
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-12-10 20:34:22 +0530

    Update transpose-matrix.c

--- transpose-matrix.c
+++ transpose-matrix.c
@@ -1,4 +1,7 @@
 /**
+
+Link: https://leetcode.com/problems/transpose-matrix/
+
  * Return an array of arrays of size *returnSize.
  * The sizes of the arrays are returned as *returnColumnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
