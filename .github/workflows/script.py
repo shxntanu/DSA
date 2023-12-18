6c9e437071cc0e493b2b726efa2021dd8eca6698
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-12-18 15:48:28 +0530

    Update script.py

--- .github/workflows/script.py
+++ .github/workflows/script.py
@@ -9,26 +9,35 @@ def update_readme(directory):
     readme_path = os.path.join(directory, "README.md")
     files_and_folders = [f for f in os.listdir(directory)]
 
-    file_links = []
-    folder_links = []
+    folder_links = {}
+    root_files = []
 
+    # Separate folders and root files
     for item in files_and_folders:
         item_path = os.path.join(directory, item)
-        if os.path.isfile(item_path):
-            if item == "README.md":
-                continue
-            
-            link_text, filename = convert_text(item)
-            file_link = f"- {filename} ([LeetCode](https://leetcode.com/problems/{link_text})) ([Solution]({item}))\n"
-            file_links.append(file_link)
-        elif os.path.isdir(item_path):
-            folder_links.append(f"📁 {item}\n")
-            folder_files = [f for f in os.listdir(item_path) if os.path.isfile(os.path.join(item_path, f))]
-            for file_name in folder_files:
-                link_text, filename = convert_text(file_name)
-                file_link = f"    - {filename} ([LeetCode](https://leetcode.com/problems/{link_text})) ([Solution]({os.path.join(item, file_name)}))\n"
-                file_links.append(file_link)
+        if os.path.isdir(item_path):
+            folder_links[item] = [f for f in os.listdir(item_path) if os.path.isfile(os.path.join(item_path, f))]
+        elif os.path.isfile(item_path) and item != "README.md":
+            root_files.append(item)
 
+    # Create folder and file links
+    content = []
+    for folder, files in folder_links.items():
+        content.append(f"📁 {folder}\n")
+        for file_name in files:
+            link_text, filename = convert_text(file_name)
+            file_link = f"    - {filename} ([LeetCode](https://leetcode.com/problems/{link_text})) ([Solution]({os.path.join(folder, file_name)}))\n"
+            content.append(file_link)
+
+    # Append root files
+    if root_files:
+        content.append("📁 Root Directory\n")
+        for file_name in root_files:
+            link_text, filename = convert_text(file_name)
+            file_link = f"    - {filename} ([LeetCode](https://leetcode.com/problems/{link_text})) ([Solution]({file_name}))\n"
+            content.append(file_link)
+
+    # Read existing README content
     with open(readme_path, "r") as readme_file:
         readme_content = readme_file.readlines()
 
@@ -40,9 +49,11 @@ def update_readme(directory):
 
     readme_file.close()
 
-    updated_content = ["## DSA\n\nCollection of DSA Problems from LeetCode and other sources (not as likely) and solved mostly in C.\n\nProblems:\n\n"] + folder_links + file_links
+    # Combine existing content with updated content
+    updated_content = ["## DSA\n\nCollection of DSA Problems from LeetCode and other sources (not as likely) and solved mostly in C.\n\nProblems:\n\n"] + content
     print(updated_content)
 
+    # Write updated content to README file
     with open(readme_path, "w") as readme_file:
         readme_file.writelines(updated_content)
 
