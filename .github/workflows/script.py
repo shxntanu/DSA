2bed6ca9c4836e631ba42db118cb7e33fd086d71
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2023-12-18 15:42:54 +0530

    Update script.py

--- .github/workflows/script.py
+++ .github/workflows/script.py
@@ -7,16 +7,27 @@ def convert_text(text):
 
 def update_readme(directory):
     readme_path = os.path.join(directory, "README.md")
-    files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    files_and_folders = [f for f in os.listdir(directory)]
 
     file_links = []
-    for file_name in files:
-        if file_name == "README.md":
-            continue
-        
-        link_text,filename = convert_text(file_name)
-        file_link = f"- {filename} ([LeetCode](https://leetcode.com/problems/{link_text})) ([Solution]({file_name}))\n"
-        file_links.append(file_link)
+    folder_links = []
+
+    for item in files_and_folders:
+        item_path = os.path.join(directory, item)
+        if os.path.isfile(item_path):
+            if item == "README.md":
+                continue
+            
+            link_text, filename = convert_text(item)
+            file_link = f"- {filename} ([LeetCode](https://leetcode.com/problems/{link_text})) ([Solution]({item}))\n"
+            file_links.append(file_link)
+        elif os.path.isdir(item_path):
+            folder_links.append(f"📁 {item}\n")
+            folder_files = [f for f in os.listdir(item_path) if os.path.isfile(os.path.join(item_path, f))]
+            for file_name in folder_files:
+                link_text, filename = convert_text(file_name)
+                file_link = f"    - {filename} ([LeetCode](https://leetcode.com/problems/{link_text})) ([Solution]({os.path.join(item, file_name)}))\n"
+                file_links.append(file_link)
 
     with open(readme_path, "r") as readme_file:
         readme_content = readme_file.readlines()
@@ -29,7 +40,7 @@ def update_readme(directory):
 
     readme_file.close()
 
-    updated_content = ["## DSA\n\nCollection of DSA Problems from LeetCode and other sources (not as likely) and solved mostly in C.\n\nProblems:\n\n"] + file_links
+    updated_content = ["## DSA\n\nCollection of DSA Problems from LeetCode and other sources (not as likely) and solved mostly in C.\n\nProblems:\n\n"] + folder_links + file_links
     print(updated_content)
 
     with open(readme_path, "w") as readme_file:
