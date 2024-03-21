08c967ca9346cd06ca1b768bc1bd2deaf09a3cfe
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2024-03-21 18:29:44 +0530

    Create reverse-linked-list.cf

---
+++ linked-list/reverse-linked-list.c
@@ -0,0 +1,11 @@
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+    while (curr != NULL) {
+        struct ListNode* nextTemp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+    return prev;
+}
