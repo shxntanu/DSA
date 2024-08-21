50256ac21ea47c5f95108aee1f081cb9ab4c5cd1
Shantanu Wable <97496261+shxntanu@users.noreply.github.com>
2024-08-21 20:18:00 +0530

    Update python.py

--- cf-templates/python.py
+++ cf-templates/python.py
@@ -1,21 +1,21 @@
-    import sys
-    input = sys.stdin.readline
-     
-    ############ ---- Input Functions ---- ############
-    def inp():
-        return(int(input()))
-    def inlt():
-        return(list(map(int,input().split())))
-    def insr():
-        s = input()
-        return(list(s[:len(s) - 1]))
-    def invr():
-        return(map(int,input().split()))
-     
-    def solve():
-        pass
-     
-    if __name__ == "__main__": 
-        t = int(input())
-        for i in range(t):
-            solve()
+import sys
+input = sys.stdin.readline
+ 
+############ ---- Input Functions ---- ############
+def inp():
+    return(int(input()))
+def inlt():
+    return(list(map(int,input().split())))
+def insr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+def invr():
+    return(map(int,input().split()))
+ 
+def solve():
+    pass
+ 
+if __name__ == "__main__": 
+    t = int(input())
+    for i in range(t):
+        solve()
